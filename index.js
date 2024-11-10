729665ae00d9036bc04a66dbc7268f07e57d4bb7
kabikira <kabikirar2@protonmail.com>
2024-11-10 17:34:25 +0900

    form送信

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/autumn", function (req, res) {
+    console.log("reqの中身", req.body);
+});
+
+app.listen(5050, function() {
+    console.log("Listening on localhost port 5050");
+});

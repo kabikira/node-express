f71b4d0155103a83f7afe6942a619a2b94eae1bb
kabikira <kabikirar2@protonmail.com>
2024-11-10 18:41:29 +0900

    PUT追加

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+const activities = require("./activities.json");
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
@@ -11,6 +12,11 @@ app.post("/autumn", function (req, res) {
     fs.writeFile(__dirname + "/data.txt", req.body.activity, function() {
         res.send("投稿完了");
     });
+});
+
+app.post("/update", function (req, res) {
+    activities[0].activity = req.body.updatedActivity;
+    res.send(activities);
 });
 
 app.listen(5050, function() {
